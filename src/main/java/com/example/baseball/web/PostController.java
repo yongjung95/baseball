49bdcf9085da21b97b197a0a6f8c77889029f22c
yongjung95 <yongjung95@gmail.com>
2024-08-31 01:55:56 +0900

    :white_check_mark: 게시글 등록 및 수정, 삭제 로직 수정
    헤더안에 있는 Jwt토큰에서 memberId를 추출하는 로직으로 변경

--- src/main/java/com/example/baseball/web/PostController.java
+++ src/main/java/com/example/baseball/web/PostController.java
@@ -5,9 +5,10 @@ import com.example.baseball.response.error.ErrorCode;
 import com.example.baseball.response.model.SingleResult;
 import com.example.baseball.response.service.ResponseService;
 import com.example.baseball.service.PostService;
+import com.example.baseball.util.JwtUtil;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.util.StringUtils;
+import org.springframework.http.HttpHeaders;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
@@ -18,24 +19,31 @@ public class PostController {
 
     private final PostService postService;
     private final ResponseService responseService;
+    private final JwtUtil jwtUtil;
 
     @PostMapping("/post")
-    public SingleResult<?> post(@RequestBody @Valid PostDto.SavePostRequestDto dto, BindingResult bindResult) {
+    public SingleResult<?> post(@RequestHeader(HttpHeaders.AUTHORIZATION) String token, @RequestBody @Valid PostDto.SavePostRequestDto dto, BindingResult bindResult) {
         if (bindResult.hasErrors()) {
             for (ObjectError allError : bindResult.getAllErrors()) {
                 return responseService.getFailParameter(allError.getDefaultMessage());
             }
         }
 
+        String memberId = jwtUtil.getMemberId(token.replace("Bearer ", ""));
+        dto.setAuthorId(memberId);
+
         return responseService.getSingleResult(postService.savePost(dto));
     }
 
     @PostMapping("/post/{postId}")
-    public SingleResult<?> post(@PathVariable("postId") Long postId, @RequestBody PostDto.UpdatePostRequestDto dto) {
+    public SingleResult<?> post(@RequestHeader(HttpHeaders.AUTHORIZATION) String token, @PathVariable("postId") Long postId, @RequestBody PostDto.UpdatePostRequestDto dto) {
         if (postId == null || postId <= 0) {
             return responseService.getFailResult(ErrorCode.POST_IS_NOT_FOUND);
         }
+        String memberId = jwtUtil.getMemberId(token.replace("Bearer ", ""));
+        dto.setAuthorId(memberId);
         dto.setPostId(postId);
+
         if (!dto.getIsUse()) {
             postService.deletePost(dto);
             return responseService.getSuccessResult();
