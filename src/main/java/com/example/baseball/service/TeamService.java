7ecc4255bb3cc80afcf566cac505bbab751d88fa
yongjung95 <yongjung95@gmail.com>
2024-09-30 00:44:30 +0900

    :white_check_mark: 폰트 수정 및 캐시 추가

--- src/main/java/com/example/baseball/service/TeamService.java
+++ src/main/java/com/example/baseball/service/TeamService.java
@@ -19,6 +19,7 @@ public class TeamService {
     private final TeamRepository teamRepository;
     private final ModelMapper modelMapper;
 
+    @Cacheable("team")
     public TeamDto.SelectTeamDto selectTeam(String teamId) {
         return modelMapper.map(teamRepository.findByTeamId(teamId), TeamDto.SelectTeamDto.class);
     }
@@ -28,7 +29,7 @@ public class TeamService {
         return teamRepository.findAll().stream().map(team -> modelMapper.map(team, TeamDto.SelectTeamDto.class)).collect(Collectors.toList());
     }
 
-    @Cacheable("team")
+    @Cacheable("teamBySymbol")
     public TeamDto.SelectTeamDto selectTeamBySymbol(String symbol) {
         return modelMapper.map(teamRepository.findBySymbol(symbol), TeamDto.SelectTeamDto.class);
     }
